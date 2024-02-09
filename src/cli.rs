2b7c2731ea1113471c2c188bb45b2ba6d8132392
calinanca99 <calinanca99@gmail.com>
2024-02-09 17:43:23 +0100

    Add version, short description and explanation for cache flag

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
+#[command(version, about = "Query top 10 Github repositories of a user")]
 pub struct Cli {
     #[arg(short, long)]
     pub username: String,
     #[arg(long)]
+    /// Read from the network even when the result is cached.
+    /// It upserts the user's repositories
     pub no_cache: bool,
     #[arg(short, long)]
     /// (Optional) Github Personal Access Token
