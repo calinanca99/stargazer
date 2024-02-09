f02585852f0716e1414cdd1be1abe497197f2442
calinanca99 <calinanca99@gmail.com>
2024-02-09 18:08:35 +0100

    Create cache file in the $HOME directory

--- src/cache.rs
+++ src/cache.rs
@@ -2,51 +2,79 @@ use std::{
     collections::HashMap,
     fs::{File, OpenOptions},
     io::{Read, Write},
+    path::PathBuf,
 };
 
+use anyhow::bail;
 use serde::{Deserialize, Serialize};
 
 use crate::models::Repositories;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug)]
 pub struct Cache {
+    data: Data,
+    path: PathBuf,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct Data {
     repositories: HashMap<String, Repositories>,
 }
 
 impl Cache {
     pub fn new() -> anyhow::Result<Self> {
-        if let Ok(mut file) = File::open(".cache") {
+        let path = get_cache_path()?;
+
+        if let Ok(mut file) = File::open(&path) {
             let mut buf = String::new();
             file.read_to_string(&mut buf)?;
 
-            let cache = serde_json::from_str::<Cache>(&buf)?;
-            Ok(cache)
+            let data = serde_json::from_str::<Data>(&buf)?;
+            Ok(Cache { data, path })
         } else {
             Ok(Cache {
-                repositories: HashMap::new(),
+                data: Data::default(),
+                path,
             })
         }
     }
 
     pub fn get_repositories(&self, username: &str) -> Option<&Repositories> {
-        self.repositories.get(username)
+        self.data.repositories.get(username)
     }
 
     pub fn upsert_repositories(&mut self, username: String, repositories: &Repositories) {
-        self.repositories.insert(username, repositories.to_vec());
+        self.data
+            .repositories
+            .insert(username, repositories.to_vec());
     }
 
     pub fn flush(&self) -> anyhow::Result<()> {
-        let s = serde_json::to_string(&self)?;
+        let s = serde_json::to_string(&self.data)?;
 
-        let mut file =
-            if let Ok(file) = OpenOptions::new().truncate(true).write(true).open(".cache") {
-                file
-            } else {
-                File::create(".cache")?
-            };
+        let mut file = if let Ok(file) = OpenOptions::new()
+            .truncate(true)
+            .write(true)
+            .open(&self.path)
+        {
+            file
+        } else {
+            File::create(&self.path)?
+        };
         file.write_all(s.as_bytes())?;
 
         Ok(())
+    }
+}
+
+fn get_cache_path() -> anyhow::Result<PathBuf> {
+    match dirs::home_dir() {
+        Some(mut home_path) => {
+            home_path.push(".stargazer.cache");
+            Ok(home_path)
+        }
+        None => {
+            bail!("Cannot find HOME directory")
+        }
     }
 }
