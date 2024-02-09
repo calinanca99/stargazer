db70650e503fbefda9ee2b90686a738de4859965
calinanca99 <calinanca99@gmail.com>
2024-02-09 17:15:48 +0100

    Define `GithubClient`

--- src/github.rs
+++ src/github.rs
@@ -1,3 +1,7 @@
+use reqwest::{
+    blocking::Client,
+    header::{AUTHORIZATION, USER_AGENT},
+};
 use serde::Deserialize;
 
 pub type GithubRepositories = Vec<GithubRepository>;
@@ -8,4 +12,35 @@ pub struct GithubRepository {
     pub html_url: String,
     pub description: Option<String>,
     pub stargazers_count: u64,
+}
+
+pub struct GithubClient {
+    token: Option<String>,
+    client: Client,
+}
+
+impl GithubClient {
+    pub fn new(token: Option<String>) -> Self {
+        Self {
+            token,
+            client: Client::default(),
+        }
+    }
+
+    pub fn get_repositories(&self, username: &str) -> anyhow::Result<GithubRepositories> {
+        let url = format!("https://api.github.com/users/{username}/repos");
+
+        let mut req = self
+            .client
+            .get(url)
+            .header(USER_AGENT, "reqwest")
+            .header("X-GitHub-Api-Version", "2022-11-28");
+
+        if let Some(token) = &self.token {
+            let bearer = format!("Bearer: {}", token);
+            req = req.header(AUTHORIZATION, bearer);
+        }
+
+        Ok(req.send()?.json::<GithubRepositories>()?)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,8 @@ use std::{
     io::Write,
 };
 
-use github::GithubRepositories;
+use github::GithubClient;
 use models::{Repositories, Repository};
-use reqwest::{
-    blocking::Client,
-    header::{AUTHORIZATION, USER_AGENT},
-};
 
 mod cache;
 use cache::Cache;
@@ -25,39 +21,26 @@ mod models;
 pub fn run(cli: Cli) -> anyhow::Result<()> {
     // Validate input
     let output = Output::try_from(cli.output)?;
+    let username = cli.username;
 
     let mut cache = Cache::new()?;
 
-    let username = cli.username;
-    let url = format!("https://api.github.com/users/{username}/repos");
-
+    let use_cache = !cli.no_cache;
     match cache.user_repositories(&username) {
-        Some(repos) if !cli.no_cache => {
+        Some(repos) if use_cache => {
             display_repositories(repos, output)?;
         }
         _ => {
-            // Get data from Github
-            let client = Client::default();
-            let mut req = client
-                .get(url)
-                .header(USER_AGENT, "reqwest")
-                .header("X-GitHub-Api-Version", "2022-11-28");
-
-            if let Some(token) = cli.token {
-                let bearer = format!("Bearer: {}", token);
-                req = req.header(AUTHORIZATION, bearer);
-            }
-
-            let mut res = req
-                .send()?
-                .json::<GithubRepositories>()?
+            let client = GithubClient::new(cli.token);
+            let mut repositories = client
+                .get_repositories(&username)?
                 .into_iter()
                 .map(Repository::from)
                 .collect::<Repositories>();
 
             // Sort
-            res.sort_by(|a, b| b.stars.cmp(&a.stars));
-            let repos = res.into_iter().take(10).collect::<Vec<_>>();
+            repositories.sort_by(|a, b| b.stars.cmp(&a.stars));
+            let repos = repositories.into_iter().take(10).collect::<Vec<_>>();
 
             // Cache
             cache.upsert(username, &repos);
